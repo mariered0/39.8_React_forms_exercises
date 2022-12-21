8f0d7d86a78c89042c9c1637f519aafa673c5e2e
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-12-20 20:43:22 -0800

    initial commit

---
+++ todo-app/src/NewTodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewTodoForm = () => {
+    return (
+        <div>
+            <h1>Todoform</h1>
+        </div>
+    )
+}
+
+export default NewTodoForm;
---
+++ todo-app/src/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewTodoForm from './NewTodoForm';
+
+const TodoList = () => {
+
+    return (
+        <div className='TodoList'>
+            <h1>TodoList</h1>
+            <NewTodoForm />
+        </div>
+    )
+}
+
+export default TodoList;
